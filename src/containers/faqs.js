8ee267374ae0b2829a0d769da708605ba8d66a6a
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-09 17:29:08 +0200

    OptOptOpt Update

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,18 +1,24 @@
-import React from "react";
-import { Accordion } from "../components";
-import faqsData from "../fixtures/faqs.json";
+import React from 'react';
+import { Accordion, OptForm } from '../components';
+import faqsData from '../fixtures/faqs';
 
 export function FaqsContainer() {
   return (
     <Accordion>
       <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      {faqsData.map((item) => (
-        <Accordion.Item key={item.id}>
-          <Accordion.Header>{item.header}</Accordion.Header>
-          <Accordion.Body>{item.body}</Accordion.Body>
-        </Accordion.Item>
-      ))}
-      <Accordion.Item></Accordion.Item>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
