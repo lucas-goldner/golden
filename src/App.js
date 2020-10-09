0230e066a9e5d80740e12068f4ca38d75978ef16
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-09 15:47:04 +0200

    Accordioooooooooooon French Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { JumbotronContainer } from "./containers/jumbotron";
 import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
