477b20ddf5c736d1e2e44cac6240e4b9d054a3a5
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-10 23:04:00 +0200

    Sign Up now onto my amazing website man Update

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,14 +4,7 @@ import "firebase/auth";
 //import {seedDatabase} from "../seed";
 
 const config = {
-  apiKey: "AIzaSyDFkZRDbVVanNjLirdlDuU3eKJn4Q1Qlmw",
-  authDomain: "golden-f9bf2.firebaseapp.com",
-  databaseURL: "https://golden-f9bf2.firebaseio.com",
-  projectId: "golden-f9bf2",
-  storageBucket: "golden-f9bf2.appspot.com",
-  messagingSenderId: "125566892612",
-  appId: "1:125566892612:web:6bff2bf9dbbbd204935a11",
-  measurementId: "G-8S3BTMQD0J",
+
 };
 
 const firebase = Firebase.initializeApp(config);
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,68 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
-export default function Singup() {
-  return <p>Hello from the sign in!</p>;
+export default function Singin() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAdress, setEmailAdress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAdress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAdress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
+  const isInvalid = password === "" || emailAdress === "";
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email Adress"
+              value={emailAdress}
+              onChange={({ target }) => setEmailAdress(target.value)}
+            />
+            <Form.Input
+              autoComplete="off"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix?
+            <Form.Link to="/signup"> Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you are not a
+            robot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
