0bba1129a0eea749ee885df189aab7d576f3ea7a
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-10 19:24:47 +0200

    Basic reactroutering Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
 import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signin, Signup } from "./pages";
 
 export default function App() {
   return (
     <Router>
-      <Route exact path="/user">
-        <p>Hello I am user</p>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path="/browse">
+        <Browse />
       </Route>
       <Route exact path={ROUTES.HOME}>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Home />
       </Route>
     </Router>
   );
---
+++ src/pages/signup.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Singup() {
+  return <p>Hello from the sign up!</p>;
+}
