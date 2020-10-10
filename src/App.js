5d8f4331e34be16e2418ec57755884454413b91f
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-10 19:15:02 +0200

    AINT´ NOTHING LIKE A FUNKY ROUTER UPDATE

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { JumbotronContainer } from "./containers/jumbotron";
 import { FooterContainer } from "./containers/footer";
 import { FaqsContainer } from "./containers/faqs";
-import * as ROUTES from "./constants/routes"
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path="/user">
+        <p>Hello I am user</p>
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </Route>
+    </Router>
   );
 }
