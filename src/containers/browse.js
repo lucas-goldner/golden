c8530d66d17432c073aa5c8e4f82c1a7d22d0138
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-12 00:49:11 +0200

    Sign Out Sign In works like a charm Update

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 import { Header, Loading } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -18,9 +20,37 @@ export function BrowseContainer({ slides }) {
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
 
-      <Header src="joker1">
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
         <Header.Feature>
-          <Header.Text>Hey</Header.Text>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Welcome to this movie it is not Joker from Persona 5. I am dearly
+            sorry I also would prefer the real Joker more but my projects are
+            already so Persona infected, that it is time to stop
+          </Header.Text>
         </Header.Feature>
       </Header>
     </>
