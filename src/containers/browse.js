857318194131e2b09a43641737935f09961a480b
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-12 00:16:29 +0200

    Lookin' cool Joker Update

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
-import {Loading} from "../components";
+import { Header, Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -15,9 +15,15 @@ export function BrowseContainer({ slides }) {
   }, [profile.displayName]);
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : <Loading.ReleaseBody/>
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src="joker1">
+        <Header.Feature>
+          <Header.Text>Hey</Header.Text>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
   );
