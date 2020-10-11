f632d3206a7b0c4585e161531e149f3aa822c618
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-11 17:14:33 +0200

    Going to cut my hair now cya later Update

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,7 @@
 import React from "react";
+import {useContent} from "../hooks";
 
-export default function Singup() {
+export default function Browse() {
+  const {series} = useContent("series");
   return <p>Hello from the browse!</p>;
 }
