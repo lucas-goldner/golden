c6c64bac76c72c27e87d67d4c0c558f45f307769
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-11 22:58:31 +0200

    Loading ahoi !

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,9 +1,24 @@
-import React, {useContext} from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
+import {Loading} from "../components";
 
 export function BrowseContainer({ slides }) {
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
-  return <SelectProfileContainer user={user} />;
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : null
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
