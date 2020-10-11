a645debc1bc77086b5e06cfbae58a82f37ddde41
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-11 16:40:54 +0200

    Everything will stop working until next commit sry

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch} from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import {
+  IsUserRedirect,
+  isUserRedirect,
+  ProtectedRoute,
+} from "./helpers/routes";
 
 export default function App() {
+  const user = null;
+
   return (
     <Router>
-      <Route exact path="/signup">
-        <Signup />
-      </Route>
-      <Route exact path="/signin">
-        <Signin />
-      </Route>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <Signup />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
