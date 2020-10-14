c5445e4a9b5a257c1a21453e90fe0a0cc38b8a1f
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-14 17:36:44 +0200

    Home Page Rework

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -76,7 +76,7 @@ export const Input = styled.input`
 `;
 
 export const Submit = styled.button`
-  background: #e50914;
+  background: #ffa611;
   border-radius: 4px;
   font-size: 16px;
   font-weight: bold;
